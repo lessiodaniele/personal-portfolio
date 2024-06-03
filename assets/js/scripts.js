4fbe7da06f992fc6899e67aa9d9a4ff1e8afdd86
Daniele Lessio <daniele.lessio00@gmail.com>
2024-06-03 18:52:02 +0200

    tolto qualche commento superfluo

--- assets/js/scripts.js
+++
@@ -1,8 +0,0 @@
-// Ottieni il riferimento al bottone
-const darkModeButton = document.getElementById('dark-mode-button');
-
-// Aggiungi un gestore di eventi per il clic sul bottone
-darkModeButton.addEventListener('click', () => {
-    // Inverti la modalità scura
-    document.body.classList.toggle('dark-mode');
-});
